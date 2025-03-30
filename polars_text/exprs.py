bb0b160f55ec172c37880da011307682fae12961
malouf <rmalouf@sdsu.edu>
2025-03-30 14:55:51 -0700

    basic pmi

--- polars_text/exprs.py
+++ polars_text/exprs.py
@@ -1,10 +1,10 @@
 import polars as pl
 
-all = ["Text"]
+from .collocations import tabulate
 
 
 @pl.api.register_expr_namespace("text")
-class Text:
+class TextExpr:
     def __init__(self, expr: pl.Expr) -> None:
         self._expr = expr
 
@@ -16,3 +16,21 @@ class Text:
 
     def ngrams(self, n: int) -> pl.Expr:
         return pl.concat_list(self._expr.shift(-i) for i in range(0, n))
+
+
+@pl.api.register_dataframe_namespace("text")
+class TextDataFrame:
+    def __init__(self, df: pl.DataFrame) -> None:
+        self._df = df
+
+    def tabulate(self, x: str, y: str) -> pl.DataFrame:
+        return tabulate(self._df, x, y)
+
+
+@pl.api.register_lazyframe_namespace("text")
+class TextLazyFrame:
+    def __init__(self, lf: pl.LazyFrame) -> None:
+        self._lf = lf
+
+    def tabulate(self, x: str, y: str) -> pl.LazyFrame:
+        return tabulate(self._lf, x, y)
