280efca5dca5ed576703bfd07f25eb482bf982d6
malouf <rmalouf@sdsu.edu>
2025-04-01 18:37:02 -0700

    noop rust plugin

--- polars_text/__init__.py
+++ polars_text/__init__.py
@@ -1,4 +1,27 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import polars as pl
+from polars.plugins import register_plugin_function
+
+from ._internal import __version__ as __version__
+
+
+if True or TYPE_CHECKING:
+    from .typing import IntoExprColumn
+
 from .exprs import *
 from .io import *
 
 __version__ = "0.1.0"
+
+LIB = Path(__file__).parent
+
+
+def noop(expr: IntoExprColumn) -> pl.Expr:
+    return register_plugin_function(
+        args=[expr],
+        plugin_path=LIB,
+        function_name="noop",
+        is_elementwise=True,
+    )
---
+++ run.py
@@ -0,0 +1,9 @@
+import polars as pl
+import polars_text as mp
+
+df = pl.DataFrame({
+    'a': [1, 1, None],
+    'b': [4.1, 5.2, 6.3],
+    'c': ['hello', 'everybody!', '!']
+})
+print(df.with_columns(mp.noop(pl.all()).name.suffix('_noop')))
