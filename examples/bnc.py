9e30111e2f2784a69d1fa08c282e2f89a85947bf
malouf <rmalouf@sdsu.edu>
2025-06-19 12:01:43 -0700

    improvements to examples

--- examples/bnc.py
+++ examples/bnc.py
@@ -10,7 +10,6 @@ from pathlib import Path
 
 import polars as pl
 from lxml import etree
-from tqdm.auto import tqdm
 
 from joblib import Parallel, delayed
 from joblib_progress import joblib_progress
@@ -27,48 +26,78 @@ def get_xml(filename):
     else:
         raise ValueError('Unknown text type')
 
+    speakers = defaultdict(list)
+    for person in doc.xpath('//person'):
+        speakers['speaker_id'].append(person.get('{http://www.w3.org/XML/1998/namespace}id'))
+        for attr in ['ageGroup', 'soc', 'sex', 'persName', 'occupation', 'dialect', 'persNote']:
+           if (t := person.get(attr)) is None:
+               speakers[attr].append(None)
+           else:
+               speakers[attr].append(t)
+
+    speakers_df = pl.DataFrame(speakers, schema={'speaker_id': pl.String,
+                                           'sex': pl.String,
+                                           'ageGroup': pl.String,
+                                           'dialect': pl.String,
+                                           #'educ': pl.String,
+                                           'soc': pl.String,
+                                           'persName': pl.String,
+                                           #'age': pl.String,
+                                           'occupation': pl.String,
+                                           'dialect': pl.String,
+                                           'persNote': pl.String
+                                           })
+
     data = defaultdict(list)
     for s in doc.xpath('//s'):
+        if u := s.xpath('ancestor::u'):
+            speaker_id = u[0].get('who')
+        else:
+            speaker_id = None
         sent_tag = 'B'
-        for item in s.xpath('.//w | .//c | .//gap | .//unclear | .//pause | .//vocal | .//event'):
-            if (item.tag == 'c' or item.tag == 'w') and item.text is None:
-                pass # print(docid, sentid, item.get('c5'))
+        for token in s.xpath('.//w | .//c | .//gap | .//unclear | .//pause | .//vocal | .//event'):
+            if (token.tag == 'c' or token.tag == 'w') and token.text is None:
+                pass
             else:
+                data['speaker_id'].append(speaker_id)
                 data['mode'].append(text_mode)
                 data['text_type'].append(text_type)
-                data['fileid'].append(docid)
+                data['file_id'].append(docid)
                 data['sent_tag'].append(sent_tag)
-                if item.tag == 'w':
-                    data['token'].append(item.text.strip())
-                    data['c5'].append(item.get('c5'))
-                    data['hw'].append(item.get('hw'))
-                    data['pos'].append(item.get('pos'))
-                elif item.tag == 'c':
-                    data['token'].append(item.text.strip())
-                    data['c5'].append(item.get('c5'))
-                    data['hw'].append(None)
+                if token.tag == 'w':
+                    data['token'].append(token.text.strip())
+                    data['c5'].append(token.get('c5'))
+                    data['lemma'].append(token.get('hw'))
+                    data['pos'].append(token.get('pos'))
+                elif token.tag == 'c':
+                    data['token'].append(token.text.strip())
+                    data['c5'].append(token.get('c5'))
+                    data['lemma'].append(None)
                     data['pos'].append('STOP')
                 else:
-                    data['token'].append(etree.tostring(item).decode())
+                    data['token'].append(f'<{token.tag}/>')
                     data['c5'].append(None)
-                    data['hw'].append(None)
+                    data['lemma'].append(None)
                     data['pos'].append(None)
             sent_tag = 'I'
 
     c = pl.DataFrame(data, schema={'token': pl.String,
-                                   'c5': pl.String,
+                                   'lemma': pl.String,
                                    'pos': pl.String,
+                                   'c5': pl.String,
+                                   'sent_tag': pl.String,
                                    'mode': pl.Categorical,
                                    'text_type': pl.Categorical,
-                                   'fileid': pl.Categorical,
-                                   'sent_tag': pl.String,
-                                   'hw': pl.String})
-    return c
+                                   'file_id': pl.Categorical,
+                                   'speaker_id': pl.String})
+    return c, speakers_df
 
 paths = sorted(list(Path('/Volumes/Corpora/bnc_xml').glob('**/*.xml')))
 with joblib_progress(total=len(paths)):
     with Parallel(n_jobs=8, return_as="generator_unordered", verbose=0) as parallel:
-        data = parallel(delayed(get_xml)(path) for path in paths)
-        c = pl.concat(data)
+        data1, data2 = zip(*parallel(delayed(get_xml)(path) for path in paths))
+        c = pl.concat(data1)
+        s = pl.concat(data2)
 
 c.write_parquet('bnc.parquet')
+s.write_parquet('bnc-speakers.parquet')
