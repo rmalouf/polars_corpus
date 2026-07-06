8210d209b0d3c8beb2f586b64eda22400434e9b4
Rob Malouf <rmalouf@sdsu.edu>
2026-07-06 12:53:51 -0700

    Move keywords scratch analysis into example notebook
    Relocate the ad-hoc chi-squared/Fisher's exact exploration out of
    commented-out code at the tail of keywords.py and into
    examples/keywords.ipynb where it can actually run. Add great_tables
    and scipy as optional dependencies for the examples extra, and drop
    the broken (undefined sns) distribution_plot stub from
    visualizations.py.

--- python/polars_corpus/visualizations.py
+++ python/polars_corpus/visualizations.py
@@ -4,23 +4,23 @@ import polars as pl
 
 from ._typing import IntoExpr, T_Frame
 
-__all__ = ["distribution_plot"]
+# __all__ = ["distribution_plot"]
 
 
-def distribution_plot(
-    df: T_Frame, expr: IntoExpr, words: str | list[str], **kwargs
-) -> Axes:
-    if isinstance(df, pl.DataFrame):
-        df = df.lazy()
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    if isinstance(words, str):
-        words = [words]
-
-    data = df.with_row_index().select("index", expr).filter(expr.is_in(words)).collect()
-
-    # TODO: fix the y column name
-    return sns.stripplot(x="index", y="token", data=data, **kwargs)
+# def distribution_plot(
+#     df: T_Frame, expr: IntoExpr, words: str | list[str], **kwargs
+# ) -> Axes:
+#     if isinstance(df, pl.DataFrame):
+#         df = df.lazy()
+#     if isinstance(expr, str):
+#         expr = pl.col(expr)
+#     if isinstance(words, str):
+#         words = [words]
+#
+#     data = df.with_row_index().select("index", expr).filter(expr.is_in(words)).collect()
+#
+#     # TODO: fix the y column name
+#     return sns.stripplot(x="index", y="token", data=data, **kwargs)
 
 
 ## mosaic plot from crosstab
